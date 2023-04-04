136ba2b807ac1ce3faaab5a8b1b340f9bcc156ab
oleniuh <maxim.oleniukh@gmail.com>
2023-04-04 15:59:20 +0100

    timer task completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,25 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 2000);
+});
+promise
+  .then(data => {
+    console.log(promise);
+    console.log('Success');
+  })
+  .then(result => {
+    console.log('Result');
+  })
+  .catch(err => {
+    console.log(promise);
+    console.log('Error');
+  })
+  .finally(end => {
+    console.log('The end!');
+    // preloader stop
+  });

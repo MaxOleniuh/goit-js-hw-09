80fcd06f804b6105d6664decc3fc34b97f67768d
MaxOleniuh <120305133+MaxOleniuh@users.noreply.github.com>
2023-04-04 22:50:51 +0000

    Deploying to gh-pages from @ MaxOleniuh/goit-js-hw-09@34ba66f799db85710117d5bac6ac07a9fefe4e5a 🚀

--- 03-promises.7bb6af22.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=new Promise((function(o,n){setTimeout((function(){o()}),2e3)}));o.then((function(n){console.log(o),console.log("Success")})).then((function(o){console.log("Result")})).catch((function(n){console.log(o),console.log("Error")})).finally((function(o){console.log("The end!")}))}();
-//# sourceMappingURL=03-promises.7bb6af22.js.map
